14f4665a0797353034c7c4b3107671290a6a0e04
jd-apprentice <contacto@jonathan.com.ar>
2025-04-05 23:44:16 -0300

    refactor: #23 more things

--- src/core/functions/readme.go
+++ src/core/functions/readme.go
@@ -10,9 +10,11 @@ import (
 func Readme(path string) types.BaseResponse {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		log.Fatalf("%s: %s\n", constants.FileNotFound, path)
+
 		return types.BaseResponse{Status: constants.QualityCheckFailed}
 	}
 
 	log.Printf("%s: %s\n", constants.FileFound, path)
+
 	return types.BaseResponse{Status: constants.QualityCheckSuccess}
 }
