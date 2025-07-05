eb1d93f12533b28d7b93e648a50f371e4cbf04f4
lalala <68082746+jd-apprentice@users.noreply.github.com>
2025-07-05 19:39:47 -0300

    feat: arg for the folder (#31)
    * feat: arg for the folder
    
    * feat: autoreview

--- src/core/functions/generate.go
+++ src/core/functions/generate.go
@@ -1,17 +1,27 @@
 package functions
 
 import (
+	"Scruticode/src/shared/constants"
 	"flag"
 	"log"
+	"os"
 )
 
 // https://gobyexample.com/command-line-flags
-func GenerateArguments(args []string) {
+func GenerateArguments(args []string) string {
 	fs := flag.NewFlagSet("generate", flag.ContinueOnError)
 	lang := fs.String("languages", "golang", "Supported languages [golang, typescript, javascript, python].")
 	platform := fs.String("platforms", "github", "Supported platforms [github, gitlab, azuredevops]")
+	folder := fs.String("folder", constants.CurrentPath, "The folder to analyze.")
 	fs.Parse(args)
 
-	log.Println("LANG:", *lang)
-	log.Println("PLATFORM:", *platform)
+	var environment = os.Getenv("GOENV")
+
+	if environment == "development" {
+		log.Println("LANG:", *lang)
+		log.Println("PLATFORM:", *platform)
+		log.Println("FOLDER:", *folder)
+	}
+
+	return *folder
 }
--- src/shared/constants/files.go
+++ src/shared/constants/files.go
@@ -3,5 +3,6 @@ package constants
 const (
 	ConfigFilePath         = "/.config/scruticode/settings.toml"
 	ReadmeFilePath         = "README.md"
+	CurrentPath            = "."
 	DefaultFilePermissions = 0644
 )
