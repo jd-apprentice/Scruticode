54bbca11bce70cfc4c635b95cc7c6570ff3abfa9
Jonathan <contacto@jonathan.com.ar>
2026-02-26 19:37:05 -0300

    include tag version

--- internal/core/functions/generate.go
+++ internal/core/functions/generate.go
@@ -2,11 +2,22 @@ package functions
 
 import (
 	"flag"
+	"fmt"
 	"os"
+
+	"Scruticode/internal/shared/constants"
 )
 
 // https://gobyexample.com/command-line-flags
 func GenerateArguments(fs *flag.FlagSet) (string, string, string, string) {
+	for _, arg := range os.Args[1:] {
+		if arg == "-v" || arg == "--version" {
+			version := constants.GetVersion()
+			fmt.Printf("Scruticode version %s\n", version)
+			os.Exit(0)
+		}
+	}
+
 	lang := fs.String("languages", "golang", "Supported languages [golang, typescript, javascript, python].")
 	platform := fs.String("platforms", "github", "Supported platforms [github, gitlab, azuredevops]")
 	directory := fs.String("directory", ".", "The directory to scan.")
